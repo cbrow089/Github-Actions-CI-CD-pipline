6486a26b20bb96310ade1e8df56d1d22e476eeb1
cbrow089 <cbrow089@yahoo.com>
2024-12-03 13:50:29 -0500

    added fix for render deployment

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,26 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    // Ensure the database connection is established
+    if (!db.db) {
+      console.error('Database connection is not established.');
+      return; // Exit early if the connection is not established
+    }
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
+    // Check if the model exists
+    if (models[modelName]) {
+      let modelExists = await db.db.listCollections({
+        name: collectionName
+      }).toArray();
+
+      if (modelExists.length) {
+        await db.db.dropCollection(collectionName);
+      }
+    } else {
+      console.error(`Model "${modelName}" does not exist.`);
     }
   } catch (err) {
-    throw err;
+    console.error('Error in cleanDb:', err);
+    throw err; // Rethrow the error for further handling
   }
-}
+};
