96c8993b76f23d708e9c2973bde94e4373fd1eb7
cbrow089 <cbrow089@yahoo.com>
2024-12-03 13:16:10 -0500

    added fix for render deployment

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,8 +1,13 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
+import {promises as fs} from 'fs';
 
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+async function loadJson() {
+  const data = await fs.readFile('./seeds/pythonQuestions.json', 'utf-8');
+  return JSON.parse(data);
+}
+const pythonQuestions = await loadJson();
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
